0cc40cbb92278b3dfcab03147d7f08f8447459f5
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-03-25 17:51:29 -0500

    Working except for PrintDecisions method

---
+++ BinarySearchDecisionTree/BinarySearchDecisionTree/Node.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryPrintDecisionsDecisionTree
+{
+    public class Node
+    {
+        string SData { get; set; } // represents value
+        Node Left { get; set; }    // left child (left subtree)
+        Node Right { get; set; }   // right child (right subtree)
+        bool goLeft;
+
+        public Node() { }
+
+        public Node(string sData)
+        {
+            SData = sData;
+        }
+
+        public void DeterminePosition(string answer)
+        {
+            answer = answer.ToLower();
+
+            switch (answer)
+            {
+                case "hot":
+                    goLeft = true;
+                    Insert(answer);
+                    break;
+                case "cold":
+                    goLeft = false;
+                    Insert(answer);
+                    break;
+                case "cake":
+                    goLeft = true;
+                    Insert(answer);
+                    break;
+                case "cookies":
+                    goLeft = false;
+                    Insert(answer);
+                    break;
+                case "white":
+                    goLeft = true;
+                    Insert(answer);
+                    break;
+                case "chocolate":
+                    goLeft = false;
+                    Insert(answer);
+                    break;
+                case "sugarcookie":
+                    goLeft = true;
+                    Insert(answer);
+                    break;
+                case "chocolate chip":
+                    goLeft = false;
+                    Insert(answer);
+                    break;
+                case "ice cream":
+                    goLeft = true;
+                    Insert(answer);
+                    break;
+                case "cheesecake":
+                    goLeft = false;
+                    Insert(answer);
+                    break;
+                case "choc":
+                    goLeft = true;
+                    Insert(answer);
+                    break;
+                case "vanilla":
+                    goLeft = false;
+                    Insert(answer);
+                    break;
+                case "strawberry":
+                    goLeft = true;
+                    Insert(answer);
+                    break;
+                case "cookies and cream":
+                    goLeft = false;
+                    Insert(answer);
+                    break;
+                default:
+                    throw new ArgumentException("Please check your arguments.");
+            }
+        }
+
+        public void Insert(string value)
+        {
+            if (SData == null)
+            {
+                SData = value;
+            }
+            if (goLeft) //If goLeft is true, the new node will go on the left
+            {
+                if (Left == null) //If the left node is null
+                {
+                    Left = new Node(value); //Assign a node to Left
+                }
+                else //If the left node is NOT null
+                {
+                    Left.Insert(value);
+                }
+            }
+            else //Otherwise, the new node will go on the right
+            {
+                if (Right == null)
+                {
+                    Right = new Node(value);
+                }
+                else
+                {
+                    Right.Insert(value);
+                }
+            }
+        }
+
+        public string PrintDecisions()
+        {
+            if(SData != null)
+            {
+                Console.WriteLine(SData);
+            }
+            if (Left != null)
+            {
+                return Left.PrintDecisions();
+            }
+            else if (Right != null)
+            {
+                return Right.PrintDecisions();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
--- BinarySearchDecisionTree/BinarySearchDecisionTree/Program.cs
+++ BinarySearchDecisionTree/BinarySearchDecisionTree/Program.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using BinaryPrintDecisionsDecisionTree;
+using System;
 
 namespace BinarySearchDecisionTree
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node n = new Node();
+
+            Console.WriteLine("Having trouble deciding what kind of dessert to have? You've come to the right place!");
+            Console.WriteLine("Do you want a 'hot' or 'cold' dessert?");
+            string hotCold = Console.ReadLine().ToLower();
+            n.DeterminePosition(hotCold);
+            
+            if (hotCold.Equals("hot"))
+            {
+                Console.WriteLine("Do you want 'cake' or 'cookies'?");
+                string cakeCookies = Console.ReadLine().ToLower();
+                n.DeterminePosition(cakeCookies);
+
+                if (cakeCookies.Equals("cake"))
+                {
+                    Console.WriteLine("'White' or 'chocolate'?");
+                    string whiteChocolate = Console.ReadLine().ToLower();
+                    n.DeterminePosition(whiteChocolate);
+                }
+                else if (cakeCookies.Equals("cookies"))
+                {
+                    Console.WriteLine("'Sugarcookie' or 'chocolate chip'?");
+                    string sugarCookieChocolate = Console.ReadLine().ToLower();
+                    n.DeterminePosition(sugarCookieChocolate);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else if (hotCold.Equals("cold"))
+            {
+                Console.WriteLine("Do you want 'ice cream' or 'cheesecake'?");
+                string iceCreamCheesecake = Console.ReadLine().ToLower();
+                n.DeterminePosition(iceCreamCheesecake);
+
+                if (iceCreamCheesecake.Equals("ice cream"))
+                {
+                    Console.WriteLine("'Choc' or 'vanilla'?");
+                    string chocVanilla = Console.ReadLine().ToLower();
+                    n.DeterminePosition(chocVanilla);
+                }
+                else if (iceCreamCheesecake.Equals("cheesecake"))
+                {
+                    Console.WriteLine("'Strawberry' or 'cookies and cream'?");
+                    string strawberryCream = Console.ReadLine().ToLower();
+                    n.DeterminePosition(strawberryCream);
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+            Console.WriteLine("Decisions: \n");
+            n.PrintDecisions();
         }
     }
 }
